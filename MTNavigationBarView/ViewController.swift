298e8cd5c8861103870c0895e168fb79ed16957e
fra-pham-minh-tien <pham.minh.tien@framgia.com>
2018-09-21 09:49:27 +0700

    Initial Commit

---
+++ MTNavigationBarView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MTNavigationBarView
+//
+//  Created by pham.minh.tien on 9/21/18.
+//  Copyright © 2018 pham.minh.tien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
